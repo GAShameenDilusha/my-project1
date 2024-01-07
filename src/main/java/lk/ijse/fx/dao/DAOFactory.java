f5cc96d3cac6c3a15202f4af6322d58cd00d5298
GAShameenDilusha <shameendilusha5678@gmail.com>
2024-01-07 11:54:32 +0530

    implement DAOFactory

--- src/main/java/lk/ijse/fx/dao/DAOFactory.java
+++ src/main/java/lk/ijse/fx/dao/DAOFactory.java
@@ -1,4 +1,48 @@
 package lk.ijse.fx.dao;
 
+import lk.ijse.fx.dao.impl.*;
+
 public class DAOFactory {
+    private static DAOFactory daoFactory;
+    private  DAOFactory(){
+    }
+
+    public static DAOFactory getDaoFactory(){
+        return (daoFactory==null)?daoFactory
+                =new DAOFactory():daoFactory;
+    }
+
+    public enum DAOTypes{
+        ATTENDENCE,CHILDREN,CHURCH,EVENT,FATHER,LOGIN,PAYMENT,REGISTRATION,SIGNUP,VEHICLE,VISIT
+    }
+
+    public SuperDAO getDAO(DAOTypes daoTypes){
+        switch (daoTypes) {
+            case ATTENDENCE:
+                return new AttendenceDAOImpl();
+            case CHILDREN:
+                return new ChildrenDAOImpl();
+            case CHURCH:
+                return new ChurchDAOImpl();
+            case EVENT:
+                return new EventDAOImpl();
+            case FATHER:
+                return new FatherDAOImpl();
+            case LOGIN:
+                return new LoginDAOImpl();
+            case PAYMENT:
+                return new PaymentDAOImpl();
+            case REGISTRATION:
+                return new RegistrationDAOImpl();
+            case SIGNUP:
+                return new SignupDAOImpl();
+            case VEHICLE:
+                return new VehicleDAOImpl();
+            case VISIT:
+                return new VisitDAOImpl();
+            default:
+                return null;
+        }
+
+    }
 }
